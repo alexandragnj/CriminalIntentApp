ae4d010acc478fb657853cdfc139032a772d13f6
alexandragnj <102973575+alexandragnj@users.noreply.github.com>
2022-04-20 10:43:54 +0300

    Chapter8 - UI Fragments and The Fragment Manager (#1)
    * Project configuration
    
    * Creating fragment, adding it tot the activity, configure ktlint, detekt
    
    * Add bindViews(), initialize crime when defining, set text enabled to false

---
+++ app/src/main/java/com/example/criminalintentapp/CrimeFragment.kt
@@ -0,0 +1,78 @@
+package com.example.criminalintentapp
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CrimeFragment : Fragment() {
+
+    private var crime: Crime = Crime()
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        bindViews(view)
+
+        dateButton.apply {
+            text = crime.date.toString()
+        }
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                //not implemented
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                //not implemented
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
+    }
+
+    private fun bindViews(view: View) {
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+    }
+}

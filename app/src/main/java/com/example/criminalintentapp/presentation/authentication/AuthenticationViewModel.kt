e695696bae81c66ba5f40f62ff64c3a76609d972
alexandragnj <102973575+alexandragnj@users.noreply.github.com>
2022-07-04 10:12:11 +0300

    Firebase Authentication (#15)
    * firebase authentication
    
    * check if user is logged in
    
    * implementing view bindind, adding view model
    
    * move to authentication package
    
    * goToMainActivity and initViewModelObservers methods, checkFields method
    
    * modify goToMainActivity
    
    * tryToLogin and tryToRegister methods, login after register
    
    * email and password vals

---
+++ app/src/main/java/com/example/criminalintentapp/presentation/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.criminalintentapp.presentation.authentication
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class AuthenticationViewModel : ViewModel() {
+
+    var userRegisterLiveData = MutableLiveData<FirebaseUser>()
+    var userLoginLiveData = MutableLiveData<FirebaseUser>()
+    var failureLiveData = MutableLiveData<String>()
+    val currentUser = FirebaseAuth.getInstance().currentUser
+
+    fun register(emailSignUp: String, passwordSignUp: String) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            emailSignUp,
+            passwordSignUp
+        ).addOnSuccessListener {
+            userRegisterLiveData.value = it.user
+        }.addOnFailureListener {
+            failureLiveData.value = it.message
+        }
+    }
+
+    fun login(emailSignIn: String, passwordSignIn: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            emailSignIn,
+            passwordSignIn
+        ).addOnSuccessListener {
+            userLoginLiveData.value = it.user
+        }.addOnFailureListener {
+            failureLiveData.value = it.message
+        }
+    }
+
+    fun checkFields(email: String, password: String): Boolean {
+        if (email == "" || password == "") {
+            return false
+        }
+        return true
+    }
+}

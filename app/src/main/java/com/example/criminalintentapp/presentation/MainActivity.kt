2b7ec60d6f3c1dc1cceaebd780b78bbe0466f027
Alexandra Ginju <alexandra.ginju@fortech.ro>
2022-07-08 09:29:22 +0300

    navigation component

--- app/src/main/java/com/example/criminalintentapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/criminalintentapp/presentation/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+     /*  val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null) {
             val fragment = CrimeListFragment.newInstance()
@@ -20,15 +20,15 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
-        }
+        }*/
     }
 
     override fun onCrimeSelected(crimeId: Int) {
-        val fragment = CrimeFragment.newInstance(crimeId)
+       /* val fragment = CrimeFragment.newInstance(crimeId)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .addToBackStack(null)
-            .commit()
+            .commit() */
     }
 }
